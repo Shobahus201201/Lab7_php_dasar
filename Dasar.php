ea8b26fa4f736e2f8c634f5f514b929ae65fc68e
Shobahus201201 <shobahussolichin33@gmail.com>
2022-05-18 15:11:09 +0700

    Menambahkan Foto

--- Dasar.php
+++ Dasar.php
@@ -9,5 +9,99 @@
 <?php
 echo "Hello World";
 ?>
+<h2>Menggunakan Variabel</h2>
+<?php
+$nim = "312010076";
+$nama = 'Shobahus Solichin';
+echo "NIM : " . $nim . "<br>";
+echo "Nama : $nama";
+?>
+<h3>Predefine Variabel</h3>
+<?php
+echo 'Selamat Datang ' . $_GET['nama'];
+?>
+<h4>Form Input</h4>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>PHP Dasar</title>
+</head>
+<body>
+<h2>Form Input</h2>
+<form method="post">
+<label>Nama: </label>
+<input type="text" name="nama">
+<input type="submit" value="Kirim">
+</form>
+<?php
+echo 'Selamat Datang ' . $_POST['nama'];
+?>
+<h5>Operator</h5>
+<?php
+$gaji = 1000000;
+$pajak = 0.1;
+$thp = $gaji - ($gaji*$pajak);
+echo "Gaji sebelum pajak = Rp. $gaji <br>";
+echo "Gaji yang dibawa pulang = Rp. $thp";
+?>
+<h5>Kondisi IF</h5>
+<?php
+$nama_hari = date("l");
+if ($nama_hari == "Sunday") {
+echo "Minggu";
+} elseif ($nama_hari == "Monday") {
+echo "Senin";
+} else {
+echo "Selasa";
+}
+?>
+<h5>Kondisi Switch</h5>
+<?php
+    $nama_hari = date("l");
+    switch ($nama_hari) {
+        case "Sunday":
+            echo "Minggu";
+            break;
+        case "Monday":
+            echo "Senin";
+            break;
+        case "Tuesday":
+            echo "Selasa";
+            break;
+        default:
+            echo "Sabtu";
+    }
+    echo"/$nama_hari";
+?>
+<h5>Perulangan For</h5>
+<?php
+echo "Perulangan 1 sampai 10 <br />";
+for ($i=1; $i<=10; $i++) {
+echo "Perulangan ke: " . $i . '<br />';
+}
+echo "Perulangan Menurun dari 10 ke 1 <br />";
+for ($i=10; $i>=1; $i--) {
+echo "Perulangan ke: " . $i . '<br />';
+}
+?>
+<h5>Perulangan While</h5>
+<?php
+echo "Perulangan 1 sampai 10 <br />";
+$i=1;
+while ($i<=10) {
+echo "Perulangan ke: " . $i . '<br />';
+$i++;
+}
+?>
+<h5>Perulangan Dow While</h5>
+<?php
+echo "Perulangan 1 sampai 10 <br />";
+$i=1;
+do {
+echo "Perulangan ke: " . $i . '<br />';
+$i++;
+} while ($i<=10);
+?>
 </body>
 </html>
